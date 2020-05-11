9f122598e3c27c0223d3ec8d3faf0d6a6b9b59d1
Ryan Lindbeck <rycharlind@gmail.com>
2020-05-11 10:23:51 -0500

    Updates

---
+++ precache-manifest.e90defa3ef3fb1f4b441d84c6db9ead7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af71a11fd6b02045e0bd28e5c6f22ba",
+    "url": "/simplestocktools/index.html"
+  },
+  {
+    "revision": "6ecbf8be15efe3f7acfa",
+    "url": "/simplestocktools/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b0acc8c8a1d146392a8e",
+    "url": "/simplestocktools/static/css/main.a892e9f5.chunk.css"
+  },
+  {
+    "revision": "6ecbf8be15efe3f7acfa",
+    "url": "/simplestocktools/static/js/2.149b7f8e.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/simplestocktools/static/js/2.149b7f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0acc8c8a1d146392a8e",
+    "url": "/simplestocktools/static/js/main.5d213b6e.chunk.js"
+  },
+  {
+    "revision": "077a34d505e508a88148",
+    "url": "/simplestocktools/static/js/runtime-main.741d24b6.js"
+  },
+  {
+    "revision": "5e2743b8e199644700290ec57b60d230",
+    "url": "/simplestocktools/static/media/github-logo.5e2743b8.svg"
+  },
+  {
+    "revision": "ed98c4a667eaa99a9b0c7b5b5761b7c9",
+    "url": "/simplestocktools/static/media/logo.ed98c4a6.svg"
+  }
+]);
